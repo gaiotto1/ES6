3be01fd971105e4f5fec804abe383464a4eee68c
Julio Gaiotto <juliogaiotto10@gmail.com>
2019-05-01 10:25:30 -0300

    utilizando template literals

--- bundle.js
+++ bundle.js
@@ -1,3 +1,12 @@
 "use strict";
 
-console.log('Teste');
+// ====== TEMPLATE LITERALS ======
+var nome = 'Julio';
+var idade = '26'; // Forma antiga
+
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.'); // Utilizando template Literals
+
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
+"use strict";
+
+console.log('const e let');
---
+++ template-literals.js
@@ -0,0 +1,10 @@
+// ====== TEMPLATE LITERALS ======
+const nome = 'Julio';
+const idade = '26';
+
+// Forma antiga
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.');
+
+// Utilizando template Literals
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`)
+
